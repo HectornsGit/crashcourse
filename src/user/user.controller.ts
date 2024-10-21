d4d0dbe0cf3a99ccf2c709927801c569e12d1ce1
HectornsGit <hector.novoa.segade@gmail.com>
2024-10-21 18:33:06 +0200

    feat: adds validation pipes and user related basic classes

---
+++ src/user/user.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/createUserDto';
+
+@Controller('user')
+export class UserController {
+  @Get(':id')
+  findAll(@Param('id') id: string) {
+    return {
+      user: {
+        id: id,
+      },
+    };
+  }
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return createUserDto;
+  }
+}
