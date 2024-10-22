17a5b7c90c5d8df107a605010dd250a39437c2f5
HectornsGit <hector.novoa.segade@gmail.com>
2024-10-22 18:45:48 +0200

    feat: adds userRepo to send db queries

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,9 +8,15 @@ import { CommentModule } from './comment/comment.module';
 import { CommentService } from './comment/comment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import config from 'ormconfig';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [UserModule, CommentModule, TypeOrmModule.forRoot(config)],
+  imports: [
+    UserModule,
+    CommentModule,
+    TypeOrmModule.forRoot(config),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService, CommentService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/createUserDto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from 'src/entities/user.entity';
 
 @Injectable()
 export class UserService {
-  findOne(id: string) {
-    return { id: id };
+  constructor(
+    @InjectRepository(User) private readonly userRepo: Repository<User>,
+  ) {}
+  async findOne(id: number) {
+    return await this.userRepo.findOne({ where: { id: id } });
   }
-  create(createUserDto: CreateUserDto) {
-    return 'User created';
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userRepo.create(createUserDto);
+    return await this.userRepo.save(user);
   }
 }
