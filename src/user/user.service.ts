97c706a19d74807d75801e77b50f3a23be5b45a7
HectornsGit <hector.novoa.segade@gmail.com>
2024-10-22 19:33:47 +0200

    chore: adds mapped types nest package

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/createUserDto';
+import { CreateUserDto, UpdateUserDto } from './dto/createUserDto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from 'src/entities/user.entity';
@@ -15,5 +15,8 @@ export class UserService {
   async create(createUserDto: CreateUserDto) {
     const user = await this.userRepo.create(createUserDto);
     return await this.userRepo.save(user);
+  }
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    return await this.userRepo.update(id, updateUserDto);
   }
 }
